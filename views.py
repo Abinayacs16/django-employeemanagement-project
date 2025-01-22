9b62611dc3e09f43f4ef013352f9e001e98cfb41
Abinayacs16 <abinayacs16@gmail.com>
2025-01-22 19:30:48 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,52 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from .models import Employee
+
+# View to display all employees
+def index(request):
+    employees = Employee.objects.all()  
+    return render(request, 'emsapp/index.html', {'emp': employees})
+
+def view_info(request, id):
+    
+        emp = Employee.objects.get(pk=id)  
+        return render(request, 'emsapp/index.html', {'emp': emp})
+   
+
+def add_employee(request):
+    if request.method == "POST":
+        empnumber = request.POST['empnumber']
+        empname = request.POST['empname']
+        empmail = request.POST['empmail']
+        empcity = request.POST['empcity']
+        # Create and save the new employee
+        Employee.objects.create(
+            empnumber=empnumber,
+            empname=empname,
+            empmail=empmail,
+            empcity=empcity
+        )
+        return redirect(reverse('index')) 
+    return redirect(reverse('index')) 
+
+def delete_employee(request, id):
+    if request.method == "POST":
+        emp = Employee.objects.get(id=id)
+        emp.delete() 
+    return redirect(reverse('index'))  
+
+def update_employee(request, id):
+    emp = Employee.objects.get(id=id)  
+    if request.method == "POST":
+        emp.empnumber = request.POST['empnumber']
+        emp.empname = request.POST['empname']
+        emp.empmail = request.POST['empmail']
+        emp.empcity = request.POST['empcity']
+ 
+        emp.save()
+        return redirect(reverse('index'))
+    return render(request, 'emsapp/index.html', {
+        'emp': Employee.objects.all(),
+        'emp_to_update': emp
+    })
