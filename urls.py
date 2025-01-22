aec3470b31a09c640d99ae2638eff9dd85fe902c
Abinayacs16 <abinayacs16@gmail.com>
2025-01-22 19:31:36 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,10 +1,23 @@
-from django.urls import path
-from . import views
+"""
+URL configuration for emsproject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
 
 urlpatterns = [
-    path('',views.index,name="index"),
-    path('view_info/<int:id>/',views.view_info, name='view_info'),
-    path('add_employee/',views.add_employee, name='add_employee'),
-    path('delete_employee/<int:id>/',views.delete_employee, name='delete_employee'),
-    path('update/<int:id>/',views.update_employee, name='update_employee'),    
-    ]
+    path('admin/', admin.site.urls),
+    path('',include('emsapp.urls'))
+]
